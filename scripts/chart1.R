dcc5199a09effb56593450c9329bf68eb50b41eb
Shane <fretws@uw.edu>
2020-05-16 15:34:36 -0700

    Create chart script example

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -1,1 +1,10 @@
 # A file with a function that returns your first chart
+library(ggplot2)
+library(dplyr)
+
+plot_1 <- function(df) {
+   diamonds_sample <- sample_n(diamonds, 1000)
+
+   ggplot(diamonds_sample) +
+      geom_point(mapping = aes(x = carat, y = price))
+}
