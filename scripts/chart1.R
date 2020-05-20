fe2b0bd15124b3f1f9300733a754f1a424c611fa
Ryan Langford <ryanclangford@icloud.com>
2020-05-20 15:28:44 -0700

    used tidyr to rearrange data

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -2,35 +2,43 @@
 
 # Change over time by travel catagory
 
-# library("dplyr")
-# library("ggplot")
-# 
-# data <- read.csv("../data/Global_Mobility_Report.csv")
-# 
-# data_test <- data %>%
-#   select(
-#     country_region, date,
-#     retail_and_recreation_percent_change_from_baseline,
-#     grocery_and_pharmacy_percent_change_from_baseline,
-#     parks_percent_change_from_baseline,
-#     transit_stations_percent_change_from_baseline,
-#     workplaces_percent_change_from_baseline,
-#     residential_percent_change_from_baseline
-#   ) %>%
-#   group_by(date, country_region) %>%
-#   summarise(
-#     r_and_r = round(mean(retail_and_recreation_percent_change_from_baseline, na.rm = TRUE)),
-#     g_and_p = round(mean(grocery_and_pharmacy_percent_change_from_baseline, na.rm = TRUE)),
-#     parks = round(mean(parks_percent_change_from_baseline, na.rm = TRUE)),
-#     transit = round(mean(transit_stations_percent_change_from_baseline, na.rm = TRUE)),
-#     workplace = round(mean(workplaces_percent_change_from_baseline, na.rm = TRUE)),
-#     residential = round(mean(residential_percent_change_from_baseline, na.rm = TRUE))
-#   )
+library("dplyr")
+library("ggplot2")
+library("tidyr")
+library("styler")
 
-# PROBLEM: Too much data to graph by date and region
-#   SOLUTIONS:
-#     - Average change per day globally
-#     - Monthly change per region(need help w/ this)
+data <- read.csv("../data/Global_Mobility_Report.csv")
+
+data_test <- data %>%
+  select(
+    -country_region_code,
+    -sub_region_1,
+    -sub_region_2
+  ) %>%
+  group_by(date, country_region) %>%
+  summarise(
+    r_and_r = round(mean(retail_and_recreation_percent_change_from_baseline, na.rm = TRUE)),
+    g_and_p = round(mean(grocery_and_pharmacy_percent_change_from_baseline, na.rm = TRUE)),
+    parks = round(mean(parks_percent_change_from_baseline, na.rm = TRUE)),
+    transit = round(mean(transit_stations_percent_change_from_baseline, na.rm = TRUE)),
+    workplace = round(mean(workplaces_percent_change_from_baseline, na.rm = TRUE)),
+    residential = round(mean(residential_percent_change_from_baseline, na.rm = TRUE))
+  ) %>%
+  gather(
+    key = travel_category,
+    value = percent_change,
+    -date,
+    -country_region
+  ) %>%
+  group_by(date, travel_category) %>%
+  summarise(
+    mean_percent_change = round(mean(percent_change, na.rm = TRUE))
+  ) %>%
+  group_by(date)
+ggplot(data = data_test) +
+  geom_line(mapping = aes(x = date, y = mean_percent_change, color = travel_category))
+  
+# NOTE* copy code block above into function below when its finished
 
 plot_1 <- function(df) {
   df <- df %>%
