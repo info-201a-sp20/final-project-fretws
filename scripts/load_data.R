b66ffd431eb4264681756555dee0ec8adbef0871
Shane <fretws@uw.edu>
2020-05-20 14:36:59 -0700

    Change to strings as factors = FALSE

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -1,4 +1,5 @@
 
-mobility <- read.csv("data/Global_Mobility_Report.csv")
+mobility <- read.csv("data/Global_Mobility_Report.csv", stringsAsFactors = F)
 
-deaths <- read.csv("data/Excess_Deaths_Associated_with_COVID-19.csv")
+deaths <- read.csv("data/Excess_Deaths_Associated_with_COVID-19.csv",
+                   stringsAsFactors = F)
