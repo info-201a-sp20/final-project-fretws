6f7c1059c7dcb0ec57e59a6e48209d07a2f63c08
Shane <fretws@uw.edu>
2020-05-20 15:48:20 -0700

    wrangle shart 1 dataset differently

--- scripts/chart1.R
+++ scripts/chart1.R
@@ -12,32 +12,36 @@ data <- read.csv("../data/Global_Mobility_Report.csv")
 data_test <- data %>%
   select(
     -country_region_code,
+    -country_region,
     -sub_region_1,
     -sub_region_2
   ) %>%
-  group_by(date, country_region) %>%
+  mutate(week = lubridate::week(lubridate::ymd(date))) %>%
+  group_by(week) %>%
   summarise(
-    r_and_r = round(mean(retail_and_recreation_percent_change_from_baseline, na.rm = TRUE)),
-    g_and_p = round(mean(grocery_and_pharmacy_percent_change_from_baseline, na.rm = TRUE)),
-    parks = round(mean(parks_percent_change_from_baseline, na.rm = TRUE)),
-    transit = round(mean(transit_stations_percent_change_from_baseline, na.rm = TRUE)),
-    workplace = round(mean(workplaces_percent_change_from_baseline, na.rm = TRUE)),
-    residential = round(mean(residential_percent_change_from_baseline, na.rm = TRUE))
+    r_and_r = round(
+      mean(retail_and_recreation_percent_change_from_baseline, na.rm = TRUE)),
+    g_and_p = round(
+      mean(grocery_and_pharmacy_percent_change_from_baseline, na.rm = TRUE)),
+    parks = round(
+      mean(parks_percent_change_from_baseline, na.rm = TRUE)),
+    transit = round(
+      mean(transit_stations_percent_change_from_baseline, na.rm = TRUE)),
+    workplace = round(
+      mean(workplaces_percent_change_from_baseline, na.rm = TRUE)),
+    residential = round(
+      mean(residential_percent_change_from_baseline, na.rm = TRUE))
   ) %>%
   gather(
     key = travel_category,
     value = percent_change,
-    -date,
-    -country_region
+    -week
   ) %>%
-  group_by(date, travel_category) %>%
-  summarise(
-    mean_percent_change = round(mean(percent_change, na.rm = TRUE))
-  ) %>%
-  group_by(date)
+  group_by(week, travel_category)
+
 ggplot(data = data_test) +
-  geom_line(mapping = aes(x = date, y = mean_percent_change, color = travel_category))
-  
+  geom_line(mapping = aes(x = week, y = mean_percent_change, color = travel_category))
+
 # NOTE* copy code block above into function below when its finished
 
 plot_1 <- function(df) {
