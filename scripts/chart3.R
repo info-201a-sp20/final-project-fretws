0e63465387c065ecb3ce12e12840915a6a04e453
Shane <fretws@uw.edu>
2020-05-20 01:00:23 -0700

    Add reminder to swap datasets in plot 3

--- scripts/chart3.R
+++ scripts/chart3.R
@@ -3,7 +3,7 @@ plot_3 <- function(data) {
    data = data %>% select(-sub_region_2)
    usa <- map_data(map = "state") %>% select(-subregion)
    df <- left_join(usa, data, by = c("region" = "sub_region_1"))
-
+   # TODO: Swap out dummy data for real summary dataset
    plot <- ggplot(df) +
       # Suppress warning from using text argument for tooltip
       suppressWarnings(
