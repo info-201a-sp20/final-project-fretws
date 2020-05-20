f3ac58f862a34a65701db8c584e5093a29506378
Shane <fretws@uw.edu>
2020-05-20 14:21:58 -0700

    Add title and axis labels

--- scripts/chart3.R
+++ scripts/chart3.R
@@ -12,6 +12,7 @@ plot_3 <- function(data) {
       geom_polygon(aes(x = long, y = lat, group = group, text = paste0(
          stringr::str_to_title(region), "\n",
          "Parks: ", parks_percent_change_from_baseline)),
-                   fill = "blue", color = "white"))
+                   fill = "blue", color = "white")) +
+      labs(x = "Longitude", y = "Latitude", title = "Mobility by State")
    ggplotly(plot, tooltip = "text")
 }
