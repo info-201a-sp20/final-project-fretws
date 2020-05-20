0d498ced8a6535d2773dea03c7f800f60c956fac
Shane <fretws@uw.edu>
2020-05-20 00:58:15 -0700

    Create plot 3 with dummy dataset

--- scripts/chart3.R
+++ scripts/chart3.R
@@ -1,1 +1,15 @@
-# A file with a function that returns your third chart
+
+plot_3 <- function(data) {
+   data = data %>% select(-sub_region_2)
+   usa <- map_data(map = "state") %>% select(-subregion)
+   df <- left_join(usa, data, by = c("region" = "sub_region_1"))
+
+   plot <- ggplot(df) +
+      # Suppress warning from using text argument for tooltip
+      suppressWarnings(
+      geom_polygon(aes(x = long, y = lat, group = group, text = paste0(
+         "State: ", region, "\n",
+         "Parks: ", parks_percent_change_from_baseline)),
+                   fill = "blue", color = "white"))
+   ggplotly(plot, tooltip = "text")
+}
