766a1537e9fb300b797e9e1d17f2640d5c245d8e
Ryan Langford <ryanclangford@icloud.com>
2020-05-20 13:36:30 -0700

    edited chart1, chart2, and index files

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -2,3 +2,5 @@
 mobility <- read.csv("data/Global_Mobility_Report.csv") %>%
    filter(country_region_code == "US") %>%
    select(-country_region_code, -country_region)
+
+deaths <- read.csv("../data/Excess_Deaths_Associated_with_COVID-19.csv")
