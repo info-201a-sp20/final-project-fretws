b924fc9f8ed2e78ea2161fea8ce07368c334f498
Shane <fretws@uw.edu>
2020-05-28 15:44:49 -0700

    begin data cleaning of page 2 plot

--- app_server.R
+++ app_server.R
@@ -1,14 +1,30 @@
 library("dplyr")
+library("lubridate")
 
-mobility <- read.csv("Global_Mobility_Report.csv")
+mobility <- read.csv("data/Global_Mobility_Report.csv")
 
 server <- function(input, output) {
 
 
    output$pg2plot <- renderplot({
       cat <- input$trav_cat
-      data <- mobility %>% select(!! as.name(cat), date)
+      data <- mobility %>%
+         select(!! as.name(cat), date) %>%
+         mutate(week = week(ymd(date))) %>%
+         group_by(week) %>%
+         summarise(avg = mean(!! as.name(cat)))
 
+      data <- mobility %>%
+         mutate(week = week(ymd(date))) %>%
+         group_by(week)
+
+      for (col in choices) {
+         # print(as.name(col))
+         df2 <- summarise(data, avg = mean(!! as.name(choices), na.rm = T))
+      }
+
+      ggplot(data) +
+         geom_smooth(aes(x = week, y = avg))
 
    })
 
