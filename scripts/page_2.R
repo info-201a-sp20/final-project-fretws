899ffe9cc7ad486b13762375182492a641f7b0d5
Shane <fretws@uw.edu>
2020-05-28 13:13:50 -0700

    Set up page 2 ui

--- scripts/page_2.R
+++ scripts/page_2.R
@@ -1,5 +1,20 @@
 # define variable for sidebar panel for second page
+side_bar <- sidebarPanel(
+   # widgets
+
+)
 
 #define variable for main panel for second page
+main_panel = mainPanel(
+   # plot(s)
+
+)
 
 #define variable to maneuver content for second page
+page_two <- tabPanel(
+   title = "Question",
+   sidebarLayout(
+      sidebarPanel = side_bar,
+      mainPanel = main_panel
+   )
+)
