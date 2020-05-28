da556a43466b68443cd4ff2221d84ed9292ec812
Shane <fretws@uw.edu>
2020-05-28 15:07:53 -0700

    load mobility data in server

--- app_server.R
+++ app_server.R
@@ -1,9 +1,14 @@
 library("dplyr")
 
+mobility <- read.csv("Global_Mobility_Report.csv")
+
 server <- function(input, output) {
 
 
    output$pg2plot <- renderplot({
+      cat <- input$trav_cat
+      data <- mobility %>% select(!! as.name(cat), date)
+
 
    })
 
