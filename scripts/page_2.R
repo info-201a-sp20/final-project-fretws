f5e0af4b680793f7845b3796a6b917d0de6406d4
Shane <fretws@uw.edu>
2020-05-28 14:16:42 -0700

    Create page 2 widgets

--- scripts/page_2.R
+++ scripts/page_2.R
@@ -1,7 +1,24 @@
 # define variable for sidebar panel for second page
 side_bar <- sidebarPanel(
    # widgets
-
+   checkboxGroupInput(
+      inputId = "trav_cat",
+      label = "Travel Category",
+      choices = c(
+         "grocery_and_pharmacy_percent_change_from_baseline",
+         "parks_percent_change_from_baseline",
+         "retail_and_recreation_percent_change_from_baseline",
+         "transit_stations_percent_change_from_baseline",
+         "workplaces_percent_change_from_baseline"
+      )
+   ),
+   dateRangeInput(
+      inputId = "date_range",
+      label = h3("Date Range"),
+      min = "2020-02-15",
+      max = "2020-05-07",
+      format = "yyyy-mm-dd"
+   )
 )
 
 #define variable for main panel for second page
