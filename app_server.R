53c10b20b4ae354ea361b24b8e4b401e84332d1e
Shane <fretws@uw.edu>
2020-05-28 15:54:21 -0700

    Comment out plot rendering that isn't complete yet

--- app_server.R
+++ app_server.R
@@ -7,27 +7,27 @@ average_table <- aggregated_table(mobility)
 server <- function(input, output) {
 
 
-   output$pg2plot <- renderPlot({
-      cat <- input$trav_cat
-      data <- mobility %>%
-         select(!! as.name(cat), date) %>%
-         mutate(week = week(ymd(date))) %>%
-         group_by(week) %>%
-         summarise(avg = mean(!! as.name(cat)))
-
-      data <- mobility %>%
-         mutate(week = week(ymd(date))) %>%
-         group_by(week)
-
-      for (col in choices) {
-         # print(as.name(col))
-         df2 <- summarise(data, avg = mean(!! as.name(choices), na.rm = T))
-      }
-
-      ggplot(data) +
-         geom_smooth(aes(x = week, y = avg))
-
-   })
+   # output$pg2plot <- renderPlot({
+   #    cat <- input$trav_cat
+   #    data <- mobility %>%
+   #       select(!! as.name(cat), date) %>%
+   #       mutate(week = week(ymd(date))) %>%
+   #       group_by(week) %>%
+   #       summarise(avg = mean(!! as.name(cat)))
+   #
+   #    data <- mobility %>%
+   #       mutate(week = week(ymd(date))) %>%
+   #       group_by(week)
+   #
+   #    for (col in choices) {
+   #       # print(as.name(col))
+   #       df2 <- summarise(data, avg = mean(!! as.name(choices), na.rm = T))
+   #    }
+   #
+   #    ggplot(data) +
+   #       geom_smooth(aes(x = week, y = avg))
+   #
+   # })
 
 
    output$chart3 <- renderPlotly({
