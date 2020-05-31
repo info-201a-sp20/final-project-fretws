d641e70249ad98b4c18fed63614873127776a039
Mariah Stanley <62573433+stanlem5@users.noreply.github.com>
2020-05-31 14:56:00 -0700

    function for page 4

--- scripts/page_4.R
+++ scripts/page_4.R
@@ -6,44 +6,37 @@ library(plotly)
 library(ggplot2)
 
 # define variable for sidebar panel for fourth page
-fourth_sidebar_content_one <- sidebarPanel(
+fourth_sidebar_content <- sidebarPanel(
    selectInput(
       "barcheck",
       inputId = "sel_reg",
       label = "Select Region",
-      choices = list("North" = "North",
-                     "NorthEast" = "NorthEast",
-                     "MidWest" = "NorthCentral",
+      choices = list("Northeast" = "Northeast",
+                     "MidWest" = "North Central",
                      "West" = "West",
-                     "East" = "East",
                      "South" = "South"
       )
-   )
-)
-
-fourth_sidebar_content_two <- sidebarPanel(
+   ),
    dateInput(
-   inputId = "sel_date",
-   label = "Select Date Range",
-   start = "01/04/2020",
-   min = "2020-02-15",
-   max = "2020-05-07",
-   format = "yyyy-mm-dd"
+      inputId = "sel_date",
+      label = "Select Date Range",
+      start = "2020-01-04",
+      min = "2020-01-01",
+      max = "2020-01-07",
+      format = "mm/dd/yyyy"
    )
 )
 
 # define variable for main panel for fourth page
 fourth_main_content <- mainPanel(
-   "Page Three",
-   plotlyOutput("bar")
+   plotOutput("pg4plot")
 )
 
 # define a variable for a panel to hold the content for the fourth page
-page_4 <- tabPanel(
+page_four <- tabPanel(
    "bar",
    titlePanel("Death Impact"),
    sidebarLayout(
       fourth_sidebar_content,
-      fourth_sidebar_content_two,
       fourth_main_content)
 )
