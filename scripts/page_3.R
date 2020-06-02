88fd36e6d49a27a897a4668cb6d28a6c7d5fa17d
Mariah Stanley <62573433+stanlem5@users.noreply.github.com>
2020-06-01 17:24:57 -0700

    fixed merge errors

--- scripts/page_3.R
+++ scripts/page_3.R
@@ -7,32 +7,20 @@ library("ggplot2")
 third_sidebar_content <-
    sidebarPanel(
       dateRangeInput(
-         "date_page_3",
-<<<<<<< HEAD
-         label = "Select Date",
-         start = as.Date("2020-02-15"),
-         end = as.Date("2020-05-07"),
-         min = as.Date("2020-02-15"),
-         max = as.Date("2020-05-07")
-=======
+         inputId = "date_page_3""
          label = h3("Date Range"),
          min = "2020-02-15",
          start = "2020-02-15",
          max = "2020-05-07",
          end = "2020-05-07",
          format = "yyyy-mm-dd"
->>>>>>> 5887aaf53fef0716a85e95eb3bafbde700dd38cd
       )
    )
 
 #define a variable for the main panel of the third page
 third_main_content <- mainPanel(
    plotlyOutput(
-<<<<<<< HEAD
       "pg3plot"
-=======
-      outputId = "chart3"
->>>>>>> 5887aaf53fef0716a85e95eb3bafbde700dd38cd
    ),
 
    p("In order to better understand how the travel habits changed by location,
