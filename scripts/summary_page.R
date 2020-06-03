146510ec84d2fa43092ede4ba7ba691b6e17b759
Mariah Stanley <62573433+stanlem5@users.noreply.github.com>
2020-06-03 11:26:34 -0700

    updated intro

--- scripts/summary_page.R
+++ scripts/summary_page.R
@@ -5,6 +5,5 @@ summary_page <- tabPanel(
    h1("The changes in mobility by state"),
    p(),
    h1("The changes in death impact by region"),
-   p("Through our analysis of the data, the annual mortality rate has risen far less significantly from the baseline
-     when asses by region instead of state. Comparing the average mobility trends for the country by region shows...")
+   p("")
 )
