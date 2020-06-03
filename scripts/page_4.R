f0e403f1a9db167718053fa0199ece0842435bf8
sumeetwaraich <62860013+sumeetwaraich@users.noreply.github.com>
2020-06-02 18:06:13 -0700

    edits to page 4

--- scripts/page_4.R
+++ scripts/page_4.R
@@ -20,7 +20,7 @@ fourth_sidebar_content <- sidebarPanel(
    dateInput(
       inputId = "sel_date",
       label = "Select Date Range",
-      min = "2020-01-01",
+      min = "2020-04-04",
       max = "2020-04-25",
       format = "yyyy-mm-dd",
       daysofweekdisabled = 0:5
@@ -29,11 +29,11 @@ fourth_sidebar_content <- sidebarPanel(
 
 # define variable for main panel for fourth page
 fourth_main_content <- mainPanel(plotOutput("pg4plot"),
-                                 p("To better understand the gravity of how travel effects the pandemic,
-                                 we used a bar chart representing the states of each region of the US.
-                                 The week can be selected to show the excess deaths by state each week
-                                 so that the user can gain a better understanding of
-                                 how the mobility trends have effected the death toll."))
+                                  p("To better understand the gravity of how travel effects the pandemic,
+                                  we used a bar chart representing the states of each region of the US.
+                                  The week can be selected to show the excess deaths by state each week
+                                  so that the user can gain a better understanding of
+                                  how the mobility trends have effected the death toll."))
 
 # define a variable for a panel to hold the content for the fourth page
 page_four <- tabPanel(
