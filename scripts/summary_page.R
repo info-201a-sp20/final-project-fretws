72aa5ddbd62f09cab4b2bcba2579ab26cb1836d2
sumeetwaraich <62860013+sumeetwaraich@users.noreply.github.com>
2020-06-03 12:43:45 -0700

    edited summary page

--- scripts/summary_page.R
+++ scripts/summary_page.R
@@ -5,7 +5,11 @@ summary_page <- tabPanel(
    h1("The changes in mobility by state"),
    p(),
    h1("The changes in death impact by region"),
-   p("Through our analysis of the data, the annual mortality rate has risen far less significantly from the baseline
-     when assessed by region instead of state. Comparing the changing numbers across each region displays clearer view 
-     on how COVID regions across the US. Comparing the average mobility trends for the country by region shows that ")
+   p("Through our analysis of the data, the annual mortality rate has risen in at different rates from the baseline 
+     across the US when assessed by region instead of state. Comparing the changing numbers across each region displays
+     clearer view on how COVID regions across the US. Comparing the average mobility trends for the country ranging from
+     January 4th to April 25, 2020. An interative chart such as this allows us to see not only regional differences in excess
+     deaths, but also state to state deaths per region. With excess death noticeably increasing across the regions form February
+     and tapering off during the month of April. Additoinally, the Southern states see the most number of excess death, reaching
+     numbers above 800 for states such as West Virginia.")
 )
