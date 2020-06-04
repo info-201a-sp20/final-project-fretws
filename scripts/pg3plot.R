6aacb8237b805947e60a2f870fe2cfe0c47956d2
Shane <fretws@uw.edu>
2020-06-03 17:20:16 -0700

    Add clarifying comments to pg3plot and move date range filtering further up in the filtering of the dataset to avoid calculating values outside the date range

--- scripts/pg3plot.R
+++ scripts/pg3plot.R
@@ -11,9 +11,13 @@ pg3plot <- function(data, date_select, category) {
   # time frame date range widget. Include a paragraph explaining the dates
   # for stay at home orders, so the user knows what time frames to look up.
 
+  # Filter filter and clean dataset
   data <- data %>%
     filter(country_region_code == "US") %>%
     filter(sub_region_1 != "") %>%
+    # filter to the given date range
+    filter(date > date_select[1], date < date_select[2]) %>%
+    # get average of all categories as another column
     mutate(all_categories =
              (grocery_and_pharmacy_percent_change_from_baseline +
              parks_percent_change_from_baseline +
@@ -21,14 +25,15 @@ pg3plot <- function(data, date_select, category) {
              transit_stations_percent_change_from_baseline +
              workplaces_percent_change_from_baseline +
              residential_percent_change_from_baseline) / 6) %>%
+    # remove useless columns
     select(-sub_region_2, -country_region, -country_region_code) %>%
     select(sub_region_1, date, !! as.name(category)) %>%
-    # filter to the given date range
-    filter(date > date_select[1], date < date_select[2]) %>%
+    # average teh selected category across the given time frame
     group_by(sub_region_1) %>%
     summarise(percent_change_from_baseline = mean(!! as.name(category),
                                                   na.rm = T))
 
+  # join with usa dataset for plotting geometry
   data <- left_join(usa, data, by = c("region" = "sub_region_1"))
 
   plot <- ggplot(data = data) +
