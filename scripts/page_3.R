a9dcd8d23d6f7b996deabafcc51a27e62c6f59fb
Shane <fretws@uw.edu>
2020-06-04 13:50:36 -0700

    Fix line that was too long

--- scripts/page_3.R
+++ scripts/page_3.R
@@ -45,8 +45,8 @@ third_main_content <- mainPanel(
    ),
 
    p("In order to better understand how the travel habits changed by location,
-     we paired a date widget with an interactive map. The stay at home order beginning__,
-     the changes can be seen by location."))
+     we paired a date widget with an interactive map. The stay at home order
+     beginning__, the changes can be seen by location."))
 
 #define a variable that takes in the contents for the third page
 page_three <- tabPanel(
