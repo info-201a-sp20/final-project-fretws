34cbf0b81f4eb1c0f5deb7ca3ee2be918ff95719
sumeetwaraich <62860013+sumeetwaraich@users.noreply.github.com>
2020-06-04 10:56:22 -0700

    summaries

--- scripts/summary_page.R
+++ scripts/summary_page.R
@@ -1,10 +1,16 @@
 summary_page <- tabPanel(
    title = h1("Conclusion"),
-   h1("The categorical changes in mobility"),
+   h1("The Categorical Changes in Mobility"),
+   p("Due to COVID-19 the US has seen tremendous changes in mobility trends across the country. As stay-at-home orders were
+     established, data was collected regarding the population's movement for a variety of activities. Those trends can be seen
+     changing from February 15th to May 7th of 2020. It was important to notice the differences in mobility for each category
+     on one graph, in reference to when when the initial data was released by Google and Apple. The Residential category is one
+     that seems to stand out due to its high rate of mobility from the baselin comapred to other catergories. By allowing customization of categories
+     and date range, we allow users to view specific information and compare information. Its important to notice that this interactive
+     chart confirms our reasoning behind the analysis of the mobility dataset. "),
+   h1("The Changes in Mobility by State"),
    p(),
-   h1("The changes in mobility by state"),
-   p(),
-   h1("The changes in death impact by region"),
+   h1("The Changes in Death Impact by Region"),
    p("Through our analysis of the data, the annual mortality rate has risen in at different rates from the baseline 
      across the US when assessed by region instead of state. Comparing the changing numbers across each region displays
      clearer view on how COVID regions across the US. Comparing the average mobility trends for the country ranging from
